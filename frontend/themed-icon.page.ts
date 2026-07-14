0fe74d2244e44177470b2724989b9ec7ac86b8eb
Antares <antares.mi@outlook.com>
2026-07-15 01:17:43 +0800

    optimize

--- frontend/themed-icon.page.ts
+++ frontend/themed-icon.page.ts
@@ -1,7 +1,7 @@
 import { $, AutoloadPage, addPage, getTheme } from "@hydrooj/ui-default";
 
 addPage(
-  new AutoloadPage("themed-icon", () => {
+  new AutoloadPage("themed-icon", void 0, () => {
     $("img.nav__logo").attr(
       "src",
       `${UiContext.cdn_prefix}nav_logo_v2_${getTheme()}.svg`,
